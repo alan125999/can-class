dc946308667bf01837d7b46a6e4d21794ad470ee
Alan Wu <alan125999@gmail.com>
2018-04-27 12:50:38 +0800

    Finish Calender

--- src/App.js
+++ src/App.js
@@ -1,71 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Timeline, TimelineEvent } from 'react-event-timeline'
-
-class ClassTimeLine extends React.Component {
-  paddingLeft(str,length){
-    if(str.length >= length) return str;
-    else return this.paddingLeft("0"  + str,length);
-  }
-  render() {
-    const lectureList = [{
-      name: "FreeBSD + Firewall",
-      time: [new Date(2018, 3, 29, 2, 0), new Date(2018, 4, 7, 22, 0), new Date(2018, 4, 10, 18, 30)],
-      lecturer: "Alan"
-    }, {
-      name: "Mail + DNS",
-      time: [new Date(2018, 4, 6, 14, 0), new Date(2018, 4, 14, 22, 0), new Date(2018, 4, 18, 18, 30)],
-      lecturer: "Setsal"
-    }, {
-      name: "網頁伺服器架設與PHP+MySQL (一)",
-      time: [new Date(2018, 4, 15, 22, 0), new Date(2018, 4, 20, 22, 0), new Date(2018, 4, 22, 18, 30)],
-      lecturer: "Anan"
-    }, {
-      name: "網頁伺服器架設與PHP+MySQL (二)",
-      time: [new Date(2018, 4, 19, 22, 0), new Date(2018, 4, 27, 14, 0), new Date(2018, 4, 31, 18, 30)],
-      lecturer: "Anan"
-    }];
-
-    let lessons = Array(0);
-    for(let i = 0; i < lectureList.length; i++){
-      for(let j = 0; j < lectureList[i].time.length; j++){
-        lessons.push(
-          <TimelineEvent 
-            // component properties
-            // title={lectureList[i].name + (j === lectureList[i].time.length - 1 ? " 正課" : (" 第 " + ( j + 1 ) + " 次驗課"))} 
-            // subtitle={"講師：" + lectureList[i].lecturer} 
-            createdAt={lectureList[i].time[j].getFullYear() + "年" 
-                     + this.paddingLeft((lectureList[i].time[j].getMonth() + 1).toString(), 2) + "月" 
-                     + this.paddingLeft(lectureList[i].time[j].getDate().toString(), 2) + "日" 
-                     + ' 星期' + '日一二三四五六'.charAt(lectureList[i].time[j].getDay()) + " " 
-                     + lectureList[i].time[j].toTimeString().slice(0, 5)} 
-            iconColor={(j === lectureList[i].time.length - 1) ? "#ff6699" : "#33cc33"}
-            container="card"
-            subtitleStyle={{color: "white"}}
-            cardHeaderStyle={{backgroundColor:( j === lectureList[i].time.length - 1 )? "#ff6699" : "#33cc33", color: "white"}}
-            // others attibutes
-            key = {i.toString() + "," + j.toString()}
-            time = {lectureList[i].time[j]}
-          >
-            課程：{lectureList[i].name + (j === lectureList[i].time.length - 1 ? " 正課" : (" 第 " + ( j + 1 ) + " 次驗課"))} <br/>
-            {"講師：" + lectureList[i].lecturer} 
-          </TimelineEvent>
-        )
-      }
-    }
-
-    lessons.sort((a, b) => {
-      return (a.props.time < b.props.time? -1 : 1);
-    });
-
-    return (
-      <Timeline>
-        {lessons}
-      </Timeline>
-    );
-  }
-}
+import Lessons from './Lessons'
+import ShadowScrollbars from './ShadowScrollbars';
 class App extends React.Component {
   render() {
     return (
@@ -77,9 +14,13 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div style={{width: "25em"}}>
-          <ClassTimeLine />
-        </div>
+        <article className="container" style={{display: "flex", flexFlow: "row wrap", justifyContent: "center", height: "20em", marginBottom: "10em"}}>
+          {Lessons.renderCalender()}
+          <ShadowScrollbars style={{ height: "447px", width: "320px" }}>
+            {Lessons.renderTimeline()}
+          </ShadowScrollbars>
+        </article>
+        <br/>
       </div>
     );
   }
---
+++ src/Lessons.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import LectureList from './lectureList.json';
+import { Timeline, TimelineEvent } from 'react-event-timeline';
+import InfiniteCalendar, {
+  Calendar,
+  withMultipleDates,
+} from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css'; // Make sure to import the default stylesheet
+
+class Lessons extends React.Component {
+  paddingLeft(str, length) {
+    if (str.length >= length) return str;
+    else return this.paddingLeft("0" + str, length);
+  }
+  myDateString(date) {
+    return date.getFullYear() + "年"
+      + this.paddingLeft((date.getMonth() + 1).toString(), 2) + "月"
+      + this.paddingLeft(date.getDate().toString(), 2) + "日"
+      + ' 星期' + '日一二三四五六'.charAt(date.getDay()) + " "
+      + date.toTimeString().slice(0, 5);
+  }
+  lessonColor(isFinal) {
+    return (isFinal === true) ? "#ff6699" : "#33cc33";
+  }
+  constructor() {
+    super()
+    this.list = Array(0);
+    for (let lessonId = 0; lessonId < LectureList.length; lessonId++) {
+      for (let lessonOrder = 0; lessonOrder < LectureList[lessonId].time.length; lessonOrder++) {
+        this.list.push({
+          id: lessonId,
+          order: lessonOrder,
+          time: new Date(
+            LectureList[lessonId].time[lessonOrder].year,
+            LectureList[lessonId].time[lessonOrder].month - 1,
+            LectureList[lessonId].time[lessonOrder].day,
+            LectureList[lessonId].time[lessonOrder].hour,
+            LectureList[lessonId].time[lessonOrder].minute
+          ),
+          isFinal: (lessonOrder === LectureList[lessonId].time.length - 1 ? true : false),
+        });
+      }
+    }
+    this.list.sort((a, b) => {
+      return (a.time < b.time ? -1 : 1);
+    });
+  }
+  renderTimeline() {
+    this.listTimeline = this.list.map((lesson) => {
+      return <TimelineEvent
+        // component properties
+        title={this.myDateString(lesson.time)}
+        iconColor={this.lessonColor(lesson.isFinal)}
+        container="card"
+        subtitleStyle={{ color: "white" }}
+        cardHeaderStyle={{ backgroundColor: this.lessonColor(lesson.isFinal), color: "white" }}
+        // others attibutes
+        key={lesson.id + "," + lesson.order}
+      >
+        課程：{LectureList[lesson.id].name} <br />
+        性質：{(lesson.isFinal === true ? "正課" : ("第 " + (lesson.order + 1) + " 次驗課"))} <br/>
+        講師：{LectureList[lesson.id].lecturer}
+      </TimelineEvent>;
+    });
+    return (
+      <Timeline>
+        {this.listTimeline}
+      </Timeline>
+    );
+  }
+  myMultipleDateInterpolation(date, selected) {
+    return selected;
+  }
+  renderCalender() {
+    let time = this.list.map((lesson) => {
+      return lesson.time;
+    })
+    return(<InfiniteCalendar
+      Component = {withMultipleDates(Calendar)}
+      selected = {time}
+      interpolateSelection={this.myMultipleDateInterpolation}
+      width = {600}
+      height = {300}
+    />);
+  };
+}
+let obj = new Lessons();
+export default obj;
