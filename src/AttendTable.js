ab16c026b875b11efd31a0d98df48bae08427331
Alan Wu <alan125999@gmail.com>
2018-04-30 05:19:03 +0800

    Version 2 - Fix laggy table scrolling, give up sticky header

--- src/AttendTable.js
+++ src/AttendTable.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
-import Paper from 'material-ui/Paper';
 
 const styles = theme => ({
   root: {
@@ -26,10 +25,7 @@ const styles = theme => ({
   head: {
     backgroundColor: "#222",
     color: theme.palette.common.white,
-    position: "sticky",
-    top: "0",
     textAlign: "center",
-    zIndex: 3,
     padding: 0,
     margin: 0,
     borderWidth: 0,
@@ -40,7 +36,6 @@ const styles = theme => ({
     textAlign: "center",
     padding: 0,
     margin: 0,
-    zIndex: 1,
     width: "100px"
   },
   id: {
@@ -48,7 +43,6 @@ const styles = theme => ({
     color: theme.palette.common.white,
     position: "sticky",
     left: "0",
-    top: 0,
     textAlign: "center",
     zIndex: 4,
     padding: 0,
@@ -58,10 +52,8 @@ const styles = theme => ({
   sub:{
     backgroundColor: "#222",
     color: theme.palette.common.white,
-    position: "sticky",
-    top: "50px",
+
     textAlign: "center",
-    zIndex: 3,
     padding: 0,
     margin: 0,
     borderWidth: 0,
@@ -79,7 +71,7 @@ class AttendTable extends React.Component {
     for (let i = 0; i < this.props.lectureList.length; i++) {
       title.push(<TableCell numeric colSpan={this.props.lectureList.length-1} className={this.props.classes.head}>{this.props.lectureList[i].name}</TableCell>);
       for (let j = 0; j < this.props.lectureList[i].time.length; j++) {
-        subTitle.push(<TableCell numeric className={this.props.classes.sub}>{(j === this.props.lectureList[i].time.length -1)? '正課' : '驗課#' + j}</TableCell>);
+        subTitle.push(<TableCell numeric className={this.props.classes.sub}>{(j === this.props.lectureList[i].time.length -1)? '正課' : '驗課#' + (j+1)}</TableCell>);
       }
     }
     title = <TableRow className={this.props.classes.head}>{title}</TableRow>;
@@ -97,7 +89,7 @@ class AttendTable extends React.Component {
     }
 
     return (
-      <Paper className={this.props.classes.root}>
+      <div className={this.props.classes.root}>
         <Table className={this.props.classes.table} height={500}>
           <TableHead>
             {title}
@@ -107,7 +99,7 @@ class AttendTable extends React.Component {
             {data}
           </TableBody>
         </Table>
-      </Paper>
+      </div>
     );
   }
 }
